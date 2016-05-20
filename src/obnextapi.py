f1fc317c66e50703cb12ee5090a90f8bc5c7bd96
Altug Bitlislioglu <altugb@Altugs-MacBook-Pro.local>
2016-05-20 21:33:32 +0200

    first obnnode implementation, obnport is missing, probably full of bugs, requires thorough testing

--- src/obnextapi.py
+++ src/obnextapi.py
@@ -13,7 +13,29 @@ lib = cdll.LoadLibrary(glob.glob(libpath)[0])
 # declare ctypes argument and return types
 # refer to: https://docs.python.org/2/library/ctypes.html
 
-# Node Interface
+
+
+
+# /** The update mask type is uint64_t, see obnsim_basic.h for the definition. That should match the definition here. */
+# /** Type to pass arguments of an event */
+OBNUpdateMask = c_ulonglong
+OBNSimTimeType = c_longlong
+
+class OBNEI_EventArg(Structure):
+    _fields_ = [("mask", OBNUpdateMask),
+                ("index", c_size_t)]
+
+#/** The event types */
+# might require a cast
+OBNEI_Event_INIT = 0        # Init of simulation
+OBNEI_Event_Y = 1          # Update Y
+OBNEI_Event_X = 2            # Update X
+OBNEI_Event_TERM = 3         # Termination of simulation
+OBNEI_Event_RCV = 4           # A port has received a message
+
+
+# =====  Node Interface  =====
+
 
 # Create a new node object, given nodeName, workspace, and optional server address.
 # Returns 0 if successful; >0 if node already exists; <0 if error.
@@ -23,5 +45,130 @@ lib = cdll.LoadLibrary(glob.glob(libpath)[0])
 lib.createOBNNode.argtypes = [c_char_p, c_char_p, c_char_p, POINTER(c_size_t)]
 lib.createOBNNode.restype = c_int
 
+ #   // Delete a node, given its ID
+ #   // Returns 0 if successful; <0 if node doesn't exist.
+ #   int deleteOBNNode(size_t id);
+
 lib.deleteOBNNode.argtypes = [c_size_t]
 lib.deleteOBNNode.restype = c_int
+
+
+ #   // Request/notify the SMN to stop, then terminate the node's simulation regardless of whether the request was accepted or not. See MQTTNodeExt::stopSimulation for details.
+ #   // Args: node ID
+ #   // Return: 0 if successful
+ #   int nodeStopSimulation(size_t nodeid);
+
+lib.nodeStopSimulation.argtypes = [c_size_t]
+lib.nodeStopSimulation.restype = c_int
+
+
+ #   // Requests the SMN/GC to stop the simulation (by sending a request message to the SMN) but does not terminate the node.
+ #   // If the SMN/GC accepts the request, it will broadcast a TERM message to all nodes, which in turn will terminate this node.
+ #   // See MQTTNodeExt::requestStopSimulation() for details.
+ #   // Args: node ID
+ #   // Return: 0 if successful
+ #   int nodeRequestStopSimulation(size_t nodeid);
+
+lib.nodeRequestStopSimulation.argtypes = [c_size_t]
+lib.nodeRequestStopSimulation.restype = c_int
+
+ #   // Check if the current state of the node is STOPPED
+ #   // Args: node ID
+ #   // Returns: true if >0, false if =0, error if <0.
+ #   int nodeIsStopped(size_t nodeid);
+
+lib.nodeIsStopped.argtypes = [c_size_t]
+lib.nodeIsStopped.restype = c_int
+
+ #   // Check if the current state of the node is ERROR
+ #   // Args: node ID
+ #   // Returns: true if >0, false if =0, error if <0.
+ #   int nodeIsError(size_t nodeid);
+
+lib.nodeIsError.argtypes = [c_size_t]
+lib.nodeIsError.restype = c_int
+
+ #   // Check if the current state of the node is RUNNING
+ #   // Args: node ID
+ #   // Returns: true if >0, false if =0, error if <0.
+ #   int nodeIsRunning(size_t nodeid);
+
+lib.nodeIsRunning.argtypes = [c_size_t]
+lib.nodeIsRunning.restype = c_int
+
+#	// Returns the current simulation time of the node with a desired time unit.
+#    // Args: node ID, the time unit, double* time
+#    // Returns: 0 if successful
+#    // *time receives the current simulation time as a double (real number)
+#    // The time unit is an integer specifying the desired time unit. The allowed values are:
+#    // 0 = second, -1 = millisecond, -2 = microsecond, 1 = minute, 2 = hour
+#    int nodeSimulationTime(size_t nodeid, int timeunit, double* T);
+    
+lib.nodeSimulationTime.argtypes = [c_size_t, c_int, POINTER(c_double)]
+lib.nodeSimulationTime.restype = c_int
+
+#    // Returns the atomic time unit, an integer in microseconds, of the simulation.
+#    // Args: node ID, OBNSimTimeType* tu
+#    // Returns: 0 if successful
+#    int nodeTimeUnit(size_t nodeid, OBNSimTimeType* tu);
+
+lib.nodeTimeUnit.argtypes = [c_size_t, POINTER(OBNSimTimeType)]
+lib.nodeTimeUnit.restype = c_int
+
+#    // Returns the current wallclock time of the node.
+#    // Args: node ID, long* time
+#    // Returns: 0 if successful
+#    // *time receives the current wallclock time as a POSIX time value.
+#    int nodeWallClockTime(size_t nodeid, int64_t* T);
+
+lib.nodeWallClockTime.argtypes = [c_size_t, c_int, POINTER(c_longlong)]
+lib.nodeWallClockTime.restype = c_int
+
+#    /* === Node simulation control interface === */
+
+#    // Get the next port event (e.g. message received) with a possible timeout.
+#    // Args: node ID, timeout (double in seconds, can be <= 0.0 if no timeout, i.e. returns immediately), unsigned int* event_type, size_t* portID
+#    // Returns: 0 if successful, >0 if timeout, <0 if other errors
+#    // If returning 0: *event_type is the type of port event (an integer cast from OBNEI_EventType, OBNEI_Event_RCV for message received); *portID is the index of the port associated with the event.
+#    int simGetPortEvent(size_t nodeid, double timeout, unsigned int* event_type, size_t* portid);
+
+lib.simGetPortEvent.argtypes = [c_size_t, c_double, POINTER(c_uint), POINTER(c_size_t)]
+lib.simGetPortEvent.restype = c_int
+
+
+#	// Runs the node's simulation until the next event, or until the node stops or has errors
+#    // Args: node ID, timeout (double in seconds, can be <= 0.0 if no timeout), unsigned int* event_type, OBNEI_EventArg* event_args
+#    // Returns: 0 if everything is going well and there is an event pending, 1 if timeout (but the simulation won't stop automatically, it's still running), 2 if the simulation has stopped (properly, not because of an error), 3 if the simulation has stopped due to an error (the node's state becomes NODE_ERROR), <0 if other error (e.g., node ID is invalid).  Check the last error message for specifics.
+#    // If returning 0: *event_type is the type of port event (an integer cast from OBNEI_EventType); *event_args are the event arguments depending on the event type (see the structure for details).
+#    int simRunStep(size_t nodeid, double timeout, unsigned int* event_type, OBNEI_EventArg* event_args);
+
+lib.simRunStep.argtypes = [c_size_t, c_double, POINTER(c_uint), c_void_p]
+lib.simRunStep.restype = c_int
+
+
+#    // Request an irregular future update.
+#    // This is a blocking call, possibly with a timeout, that waits until it receives the response from the SMN or until a timeout.
+#    // Args: node ID, future time (integer value in the future), update mask of the requested update, timeout (double, can be <= 0)
+#    // Returns: status of the request: 0 if successful (accepted), -1 if timeout (failed), -2 if request is invalid, >0 if other errors (failed, see OBN documents for details).
+#    int simRequestFutureUpdate(size_t nodeid, OBNSimTimeType t, OBNUpdateMask mask, double timeout);
+
+lib.simRequestFutureUpdate.argtypes = [c_size_t, OBNSimTimeType, OBNUpdateMask, c_double]
+lib.simRequestFutureUpdate.restype = c_int
+
+
+#api_nodeIsStopped = c_void_p.in_dll(lib, "nodeIsStopped")
+#api_nodeIsError = c_void_p.in_dll(lib, "nodeIsError")
+#api_nodeIsRunning = c_void_p.in_dll(lib, "nodeIsRunning")
+# =====  Port Interface  =====
+
+
+#    /* === Misc === */
+#    // Returns the maximum ID allowed for an update type.
+#    int maxUpdateID();
+
+lib.maxUpdateID.argtypes = []
+lib.maxUpdateID.restype = c_int
+
+max_blockid = lib.maxUpdateID()
+
+
--- src/obnnode.py
+++ src/obnnode.py
@@ -1,18 +1,221 @@
+#!/usr/bin/env python
+
 from ctypes import *
 from obnextapi import *
+from warnings import *
+from datetime import datetime
+# check raising errors!!!!
+
+# a callback keeps a function handle and a tuple of custom arguments and keyword agruments
+##usage 
+# def foo(*args): print sum(args)
+# fcallb = Callback(foo,1,2,3)
+# fcall() 
+class OBNCallback(object):
+	def __init__(self, function, *args, **kwargs):
+		self.function = function
+		self.args = args
+		self.kwargs = kwargs
+
+	def __call__(self):
+		# call function with optional arguments and keyword arguments
+		self.function(*self.args, **self.kwargs)
+
+
+
 
 class OBNNode(object):
 
-	def __init__( self, name, workspace, server ):
-		id = c_size_t()
-		result = lib.createOBNNode(name,workspace,server,byref(id))
+	def __init__(self, name, workspace, server ):
+		nodeid = c_size_t()
+		result = lib.createOBNNode(name,workspace,server,byref(nodeid))
 		if result != 0 :
 			raise ValueError('OBN node could not be created',res)
 
-		self.id = id.value
+		self.valid = True
+		self.id = nodeid.value
+
+		# add empty callback attributes
+		self.block_output_cb = {}
+		self.block_state_cb = {}
+		self.init_cb = None
+		self.term_cb = None
 
 
-	def delete( self ):
+
+	def delete(self ):
 
 		lib.deleteOBNNode(self.id)
+		self.valid = False
 		print("Node deleted")
+
+	# set callbacks
+	def on_block_output(self, function, blkid, *args, **kwargs):
+		assert(node.valid), 'Node is not valid'
+		assert(blkid > 0 and blkid <= max_blockid), "Invalid computation block ID."
+
+		self.block_output_cb[blkid] = OBNCallback(function,*args,**kwargs)
+
+	def on_state_output(self, function, blkid, *args, **kwargs):
+		assert(node.valid), 'Node is not valid'
+		assert(blkid > 0 and blkid <= max_blockid), "Invalid computation block ID."
+
+		self.block_state_cb[blkid] = OBNCallback(function,*args,**kwargs)
+
+	def on_init(self, function, *args, **kwargs):
+		assert(node.valid), 'Node is not valid'
+
+		self.init_cb = OBNCallback(function,*args,**kwargs)
+
+	def on_term(self, function, *args, **kwargs):
+		assert(node.valid), 'Node is not valid'
+
+		self.term_cb = OBNCallback(function,*args,**kwargs)
+	# callback execution
+	def do_updatey(self,mask):
+		for blkid, callback in self.block_output_cb:
+			if mask == 0:
+				break
+			if (mask & (1 << blkid)) != 0:
+				# If the id is in the mask, run the callback
+				callback()
+    			# ^  bitwise xor
+    			mask ^= (1 << blkid) # reset that bit
+
+	def do_updatex(self,mask):
+		for blkid, callback in self.block_state_cb:
+			if mask == 0:
+				break
+			if (mask & (1 << blkid)) != 0:
+				# If the id is in the mask, run the callback
+				callback()
+				# ^  bitwise xor
+				mask ^= (1 << blkid) # reset that bit
+
+	# Run simulation
+	# Returns: 1 if timeout; 2 if the simulation stopped properly; 3 if stopped with an error
+
+	def run(self, timeout = -1.0, stopIfTimeout = True):
+		assert(node.valid), 'Node is not valid'
+
+		event_type = c_uint()
+		event_args = OBNEI_EventArg()
+
+		result = 0
+
+		while result == 0:
+			result = lib.simRunStep(node.id, timeout, byref(event_type), byref(event_args))
+
+			if result == 0:
+				# there is an event
+				if event_type == OBNEI_Event_Y:
+					self.do_updatey(event_args.mask)
+				elif event_type == OBNEI_Event_X:
+					self.do_updatex(event_args.mask)
+				elif event_type == OBNEI_Event_INIT:
+					self.init_cb()
+				elif event_type == OBNEI_Event_TERM:
+					self.term_cb()
+				# elif event_type == OBNEI_Event_RCV:
+				# Port's RCV event
+				else:
+					raise ValueError("Internal error: Unknown event type.",event_type)
+			elif result == 1:
+				warn("Simulation has timed out.")
+				if stopIfTimeout:
+					print("I should be stopping the sim immediately")
+			elif result == 2:
+				print("I should be stopping properly")
+			elif result == 3:
+				warn("Simulation has stopped due to an error.")
+			else:
+				raise ValueError("Internal error: Unknown event type.",result)
+
+		return result
+
+	def stop(self, stopnow = False):
+		assert(node.valid), 'Node is not valid'
+
+		if stopnow:
+			result = lib.nodeStopSimulation(self.id)
+		else:
+			result = lib.nodeRequestStopSimulation(self.id)
+
+		return (result == 0)
+
+	# Requests a triggering (i.e., update) of certain blocks at a future time.
+	# Returns the status of the request:
+	# 0 if successful (accepted), -1 if timeout (failed), -2 if request is invalid, >0 if other errors (failed, see OBN documents for details).
+	def schedule(self, blocks_mask, simtime, timeout = -1.0, *args):
+		assert(node.valid), 'Node is not valid'
+		# optional use
+		# schedule(self, blocks_mask, simtime, timeunit, timeout, timeout)
+
+		if args:
+			# Here, the time is given in simulation time with a given unit (default = seconds) from the beginning of the simulation.
+			# Valid units are :s, :m, :h, :ms, :us
+			# This case converts the future time to the atomic clock ticks and call the default method
+			atomictu = self.timeunit()
+			timeunit = args[0]
+			tu_scale = {'s':1e6, 'ms':1e3, 'us':1, 'm':60*1e6, 'h':60*60*1e6}
+			# Convert t to clock ticks
+			clkticks = simtime*tu_scale[timeunit] / atomictu
+			self.schedule(blocks_mask, clkticks, timeout)
+		elif type(simtime) is datetime:
+				# Duration until the requested future time, in milliseconds
+				# Here, the time is given in wallclock time.
+				# This case converts the future time to the atomic clock ticks and call the default method
+				dt = simtime - self.wallclock_time()
+				assert(dt.total_seconds()>0), "Requested time must be strictly in the future."
+				t = self.simtime('ms') + dt.total_seconds()*1e3
+				self.schedule(blocks_mask, clkticks, timeout)
+		else:
+			# Here the time is in the clock ticks, i.e., the number of atomic time units from the beginning of the simulation.
+			return lib.simRequestFutureUpdate(self.id, blocks_mask, simtime, timeout) 
+		
+
+
+	# Get current simulation time in given unit
+	# Possible units as strings: us, ms, s, m, h
+	def sim_time(self, unit = 's'):
+		assert(node.valid), 'Node is not valid'
+		# Time unit: 0 = second, -1 = millisecond, -2 = microsecond, 1 = minute, 2 = hour
+		timeunits = {'s':0, 'ms':-1, 'us':-2, 'm':1, 'h':2}
+		assert(unit in timeunits), 'Invalid time unit'
+
+		curtime = c_double(0.0)
+		result = lib.nodeSimulationTime(self.id, timeunits[unit], byref(curtime))
+		if result != 0:
+			raise ValueError("Error querying the simulation time [$result]: ")
+		return curtime.value
+
+	# Returns the atomic time unit as an integer in microseconds
+	def timeunit(self):
+		assert(node.valid), 'Node is not valid'
+
+		tu = OBNSimTimeType(0)
+		result = lib.nodeTimeUnit(node.id,byref(tu))
+		if result != 0: raise ValueError("Error querying the time unit [$result]: ")
+		return tu.value
+
+	# Get current wallclock time as Python's DateTime
+	def wallclock_time(self):
+		assert(node.valid), 'Node is not valid'
+		unixtime = c_longlong(0)
+		result = lib.nodeWallClockTime(self.id, byref(unixtime))
+		if result != 0:
+			raise ValueError("Error querying the wall clock time [$result]: ")
+
+		# needs testing
+		return datetime.fromtimestamp(unixtime.value)
+
+	# Internal function to check state of node
+	def _is_state(self, apifun):
+		assert(node.valid), 'Node is not valid'
+
+		result = apifun(self.id)
+		if result < 0 :  raise ValueError("Error querying node's state [$result]: ")
+	# Check node's state
+	def isstopped(self): return self._is_state(lib.nodeIsStopped)
+	def isrunning(self): return self._is_state(lib.nodeIsRunning)
+	def iserror(self): return self._is_state(lib.nodeIsError)
